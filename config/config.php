40024b11ea3b1d11c1dbe2137a4607d46500694e
bambamboole <manuel.christlieb@xentral.com>
2024-02-21 21:55:58 +0100

    remove config comment

--- config/config.php
+++ config/config.php
@@ -1,8 +1,5 @@
 <?php declare(strict_types=1);
 
-/*
- * You can place your custom package configuration in here.
- */
 return [
     'dump_translations' => env('DUMP_TRANSLATIONS', false),
     'dumper' => \Bambamboole\LaravelTranslationDumper\TranslationDumper::class,
